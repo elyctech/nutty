f516a4a0b1a5154250bfb10a9ec94a6b651f0546
Cyle Dawson <dawsonc623@gmail.com>
2016-12-13 18:26:45 +0000

    Adds user story creation to Nutty Service.

--- src/lib/nutty_service.ts
+++ src/lib/nutty_service.ts
@@ -1,8 +1,11 @@
-import Project  from "./project";
+import Project    from "./project";
+import UserStory  from "./project/user_story";
 
 interface NuttyService
 {
   createProject(description: string): Project;
+
+  createUserStory(description: string): UserStory;
 }
 
 export default NuttyService;
---
+++ src/lib/project/user_story.ts
@@ -0,0 +1,6 @@
+interface UserStory
+{
+  getDescription(): string;
+}
+
+export default UserStory;
