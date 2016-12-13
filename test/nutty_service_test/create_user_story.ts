06c4eb092e4437517a7cf69232883fa5bbdf2ab6
Cyle Dawson <dawsonc623@gmail.com>
2016-12-13 16:55:14 +0000

    Adds tests for story#3 acceptance criteria.

---
+++ test/nutty_service_test/create_user_story.ts
@@ -0,0 +1,3 @@
+export { default as acceptsADescription }                 from "./create_user_story/accepts_a_description";
+export { default as providesAUserStoryObject }            from "./create_user_story/provides_a_user_story_object";
+export { default as raisesExceptionForEmptyDescription }  from "./create_user_story/raises_exception_for_empty_description";
---
+++ test/nutty_service_test/create_user_story/raises_exception_for_empty_description.ts
@@ -0,0 +1,28 @@
+import NuttyService from "../../../src/lib/nutty_service";
+import Result       from "../../result";
+import UserStory    from "../../../src/lib/project/user_story";
+
+function raisesExceptionForEmptyDescription(nuttyService: NuttyService, result: Result)
+{
+  try
+  {
+    let userStory1 : UserStory;
+
+    userStory1 = nuttyService.createUserStory("");
+
+    result.fail("Excepted exception stating 'Description cannot be empty' but got no exception");
+  }
+  catch (exception)
+  {
+    if (exception.message === "Description cannot be empty")
+    {
+      result.pass();
+    }
+    else
+    {
+      this.exception(exception);
+    }
+  }
+}
+
+export default raisesExceptionForEmptyDescription;
