1660cac7ec513d1d42b594638bc878a053ed75ac
Cyle Ven Dawson <dawsonc623@gamil.com>
2016-12-22 22:25:42 +0000

    Adds tests for setting user story descriptions.
    Story #4

--- test/nutty_service_test.ts
+++ test/nutty_service_test.ts
@@ -1,10 +1,22 @@
 import StandardNuttyService     from "../src/lib/standard/nutty_service";
 
+import * as testBuildUserStory  from "./nutty_service_test/build_user_story";
 import * as testCreateProject   from "./nutty_service_test/create_project";
 import * as testCreateUserStory from "./nutty_service_test/create_user_story";
 
 export default function(test: Function): void
 {
+  //--- buildUserStory
+  test(
+    "The developer can give the User Story a description",
+    testBuildUserStory.setsDescription.bind(null, new StandardNuttyService())
+  );
+
+  test(
+    "An exception is raised stating a description is required if the developer provides an empty description",
+    testBuildUserStory.raisesExceptionForEmptyDescription.bind(null, new StandardNuttyService())
+  );
+
   //--- createProject
   test(
     "The developer can give the Project a description",
