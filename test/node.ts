94beb21a946ffb7604c182a0e07557db557ecad5
Cyle Ven Dawson <dawsonc623@gmail.com>
2017-01-19 11:03:28 -0600

    Adds Array.prototype.includes polyfill.

--- test/node.ts
+++ test/node.ts
@@ -1,3 +1,54 @@
+// Polyfill for Node 4/5
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, 'includes', {
+    value: function(searchElement: any, fromIndex: number | undefined) {
+
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If len is 0, return false.
+      if (len === 0) {
+        return false;
+      }
+
+      // 4. Let n be ? ToInteger(fromIndex).
+      //    (If fromIndex is undefined, this step produces the value 0.)
+      var n = fromIndex | 0;
+
+      // 5. If n ≥ 0, then
+      //  a. Let k be n.
+      // 6. Else n < 0,
+      //  a. Let k be len + n.
+      //  b. If k < 0, let k be 0.
+      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      // 7. Repeat, while k < len
+      while (k < len) {
+        // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+        // b. If SameValueZero(searchElement, elementK) is true, return true.
+        // c. Increase k by 1.
+        // NOTE: === provides the correct "SameValueZero" comparison needed here.
+        if (o[k] === searchElement) {
+          return true;
+        }
+        k++;
+      }
+
+      // 8. Return false
+      return false;
+    }
+  });
+}
+
 //--- Testing API
 
 interface Result
@@ -22,9 +73,9 @@ interface Result
 let result      : Result,
     resultCount : number,
     runTest     : Function;
-    
+
 result = {
-  exceptions  : 0, 
+  exceptions  : 0,
   failures    : 0,
   passes      : 0,
   report      : "",
