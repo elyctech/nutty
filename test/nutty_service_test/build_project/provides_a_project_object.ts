f5a7822fd18b9a2c805e8a4afcfd6780f5a742c6
Cyle Ven Dawson <dawsonc623@gmail.com>
2017-01-22 10:43:17 -0600

    Adds test to ensure a Project is given to the user
    Story #2

---
+++ test/nutty_service_test/build_project/provides_a_project_object.ts
@@ -0,0 +1,17 @@
+import NuttyService from "../../../src/lib/nutty_service";
+import Result       from "../../result";
+import Project      from "../../../src/lib/project";
+
+function raisesExceptionForEmptyDescription(nuttyService: NuttyService, result: Result)
+{
+  let project  : Project;
+
+  project = nuttyService.buildProject()
+                        .setDescription("A description")
+                        .getProject();
+
+  // Since strict null checks are enabled, as long as it makes it here project has to be an instance of a project
+  result.pass();
+}
+
+export default raisesExceptionForEmptyDescription;
